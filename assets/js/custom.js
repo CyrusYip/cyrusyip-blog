ee19857a7cb2f272681b2728732d309bc63028db
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2023-11-01 01:02:59 +0800

    feat: Click to show language list for Vimium C

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,14 @@
 const custom = () => {
-  console.log("custom.js imported!");
+  // Click to show language list for Vimium C
+  const languageSelector = document.querySelector(".menu-item-lang");
+  const submenu = document.querySelector(".submenu");
+  languageSelector.addEventListener("click", () => {
+    if (getComputedStyle(submenu).visibility === "hidden") {
+      submenu.classList.add("visible");
+    } else {
+      submenu.classList.remove("visible");
+    }
+  });
 };
 
-export default custom
+export default custom;
