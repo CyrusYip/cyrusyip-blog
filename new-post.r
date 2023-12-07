b59dd43d8517dbcedb42a04eb378bc298ef20d24
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2023-12-08 00:26:15 +0800

    conda: add blogdown 1.18

--- new-post.r
+++ new-post.r
@@ -1,3 +1,3 @@
 #!/usr/bin/env Rscript
 blogdown:::new_post_addin()
-blogdown:::serve_site()
+#blogdown:::serve_site()
---
+++ new-post.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PATH="./node_modules/hugo-extended/vendor:$PATH"
+./new-post.r
