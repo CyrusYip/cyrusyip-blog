020e4f7b7cead086f2cfa584af4fd95a836181fb
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2023-12-31 03:25:27 +0800

    cms: use path module instead of regex

--- rename.js
+++ rename.js
@@ -4,17 +4,17 @@
 // Rename a file according to date and slug
 const { rename } = require("node:fs");
 const { argv } = require("node:process");
+const { extname, dirname, join } = require("node:path");
 
 const arguments = argv;
 if (arguments && arguments.length > 0) {
-  const frontMatter = JSON.parse(arguments[4]);
+  const { date, slug } = JSON.parse(arguments[4]);
   const oldPath = arguments[3];
-  const extension = oldPath.match(/.+(\..+)$/)[1];
-  const newFilename = frontMatter.date + "-" + frontMatter.slug + extension;
-  const newPath = oldPath.replace(/(.+\/)(.+)/, `$1${newFilename}`);
+  const newFilename = date + "-" + slug + extname(oldPath);
+  const newPath = join(dirname(oldPath), newFilename);
   if (oldPath === newPath) {
     console.log("No need to rename file");
-    return null;
+    return;
   }
   rename(oldPath, newPath, (err) => {
     if (err) throw err;
@@ -22,4 +22,5 @@ if (arguments && arguments.length > 0) {
   });
   // console.log("Old path: " + oldPath);
   // console.log("New path: " + newPath);
+  // console.log("New filename: " + newFilename);
 }
